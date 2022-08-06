b3a18388ff048f9d607453df0a28a54e3b20f042
Binh Do <binh@worldfront.co>
2022-08-07 10:27:03 +1200

    Add message and validation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,4 +7,26 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
+
+  def new
+    @article = Article.new
+  end
+
+  def create 
+    #render plain: params[:article] ==> return a plain text of hash "article"=>{"title"=>"hello rails", "description"=>"hello rails"} with the top-level key is :article
+    # However, we can't pass params[:article] as Article.new(params[:article])
+    # We need to whitelist this param to be accepted only sub-key are :title and :description fields because those are the one we allow and use in our application
+    @article = Article.new(params.require(:article).permit(:title, :description))
+    if @article.save
+      #render plain: @article.inspect
+      # article_path(@article) will query article.id to redirect to it's show.html
+      # another way is to use "redirect_to @article"
+      flash[:notice] = "Article was created successfully"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+      
+    end
+  end
+  
 end
