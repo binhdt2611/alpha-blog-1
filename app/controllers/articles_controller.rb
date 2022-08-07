2965e1720378adf8596f527204fa2c7a21c9c8a8
Binh Do <binh@worldfront.co>
2022-08-08 11:09:05 +1200

    #Refactor and partials the code

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,8 @@
 class ArticlesController < ApplicationController
-  
+  # before_action will perform this set_article action before we do anything of these method below 
+  # only: [list of :symbol] to affect only methods we want
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
   def show
-    @article = Article.find(params[:id])
   end
 
   def index
@@ -13,14 +14,13 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def create 
     #render plain: params[:article] ==> return a plain text of hash "article"=>{"title"=>"hello rails", "description"=>"hello rails"} with the top-level key is :article
     # However, we can't pass params[:article] as Article.new(params[:article])
     # We need to whitelist this param to be accepted only sub-key are :title and :description fields because those are the one we allow and use in our application
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
     if @article.save
       #render plain: @article.inspect
       # article_path(@article) will query article.id to redirect to it's show.html
@@ -33,8 +33,7 @@ class ArticlesController < ApplicationController
   end
   
   def update
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
+    if @article.update(article_params)
       flash[:notice] = "Article was created successfully"
       redirect_to @article
     else
@@ -43,8 +42,19 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     redirect_to articles_path
+  end
+
+  # private means any actions or methods put below it are only available to its controller
+  # They're not gonna be available for use outside controller
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
   end
 end
